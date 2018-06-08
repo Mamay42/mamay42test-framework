cb590b033b59cfddd9546438c642eb8dc6f68f6f
Oleh Laba <laba.ye.oleh@gmail.com>
2018-06-08 19:47:02 +0300

    add allure reports

--- src/test/java/TestRegistration.java
+++ src/test/java/TestRegistration.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
 
 public class TestRegistration extends TestBase {
 
@@ -15,6 +16,7 @@ public class TestRegistration extends TestBase {
 
 
     @Test(dataProvider = "userData")
+    @Description("If test fail, change user data")
     public void testRegistration(String firstName, String lastName, String country,
                                  String month, String day, String year, String phoneNumber,
                                  String username, String email, String description,
--- src/test/java/TestSelectable.java
+++ src/test/java/TestSelectable.java
@@ -1,20 +1,26 @@
 import common.TestBase;
+import ru.yandex.qatools.allure.annotations.Description;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageobject.sideMenuPages.SelectablePage;
+import ru.yandex.qatools.allure.annotations.Step;
 
 public class TestSelectable extends TestBase {
 
     SelectablePage selectablePage;
+
     @BeforeClass
     public void openSelectablePage() {
         selectablePage = homePage.clickSelectablebleButton();
     }
 
+
     @Test
+    @Description("This test will choose  1 , 5, 6, 11 elements")
     public void clickGridElements() {
         selectablePage.clickDisplayAsGridButton();
         new Actions(driver)
